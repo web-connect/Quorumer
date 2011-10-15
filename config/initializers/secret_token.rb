f8ee47686f61099f4728a9a01241f30e5e553916
Justin Turner <justinrayturner@gmail.com>
2011-10-14 23:06:57 -0500

    Adding ruby installation

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+QuorumerCom::Application.config.secret_token = 'b7be6511faebf1963aa974beba9999c003ffe3e8c40cbc6a1dfe0ddca85fabd9c3676154b0d1ceb96e687456025efce1f36466196649c15eb5a93b4101dad869'
